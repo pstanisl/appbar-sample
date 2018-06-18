62f112915a60428b21fa9d917779cdc2394a8036
pstanisl <petr.stanislav@gmail.com>
2018-06-18 21:16:08 +0200

    Removed unused code

--- app/src/main/java/cz/pstanisl/appbarexample/ui/shared/Extensions.kt
+++ app/src/main/java/cz/pstanisl/appbarexample/ui/shared/Extensions.kt
@@ -1,12 +1,7 @@
 package cz.pstanisl.appbarexample.ui.shared
 
-import android.content.Context
 import android.support.v4.app.Fragment
 import android.widget.Toast
-
-fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, message, duration).show()
-}
 
 fun Fragment.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this.context, message, duration).show()
