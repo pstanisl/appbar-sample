01850f83b50676d103679e8026984e3df651e740
pstanisl <petr.stanislav@gmail.com>
2018-08-21 16:00:28 +0200

    Favorite icons

--- app/src/main/java/cz/pstanisl/appbarexample/ui/inbox/InboxAdapter.kt
+++ app/src/main/java/cz/pstanisl/appbarexample/ui/inbox/InboxAdapter.kt
@@ -1,7 +1,10 @@
 package cz.pstanisl.appbarexample.ui.inbox
 
+import android.content.res.ColorStateList
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
 import cz.pstanisl.appbarexample.R
 import cz.pstanisl.appbarexample.model.Message
@@ -28,8 +31,18 @@ class InboxAdapter: RecyclerView.Adapter<InboxAdapter.InboxViewHolder>(), AutoUp
 
     class InboxViewHolder constructor(itemView: View): RecyclerView.ViewHolder(itemView) {
 
-        fun bind(message: Message) = with(itemView) {
-            from.text = message.from
+        fun bind(msg: Message) = with(itemView) {
+            from.text = msg.from
+            subject.text = msg.subject
+            message.text = msg.message
+            timestamp.text = msg.timestamp
+
+            val favoriteIconResId = if (msg.isImportant) R.drawable.ic_star_black_24dp else R.drawable.ic_star_border_grey600_24dp
+            val favoriteColorId = if (msg.isImportant) R.color.favorite else R.color.google_grey600
+
+            favorite.setImageResource(favoriteIconResId)
+            DrawableCompat.setTint(favorite.drawable, ContextCompat.getColor(context, favoriteColorId))
+//            favorite.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, favoriteColorId))
         }
 
     }
